1f17fb5b13df05f63af536a86aff43c771d69080
Chris <cciric44@gmail.com>
2020-05-19 22:39:51 -0400

    first commit, SQLite class and methods created

---
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -0,0 +1,37 @@
+package com.hfad.starbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends Activity {
+
+    public static final String EXTRA_DRINKID = "drinkId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        //get the drink from the intent
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        //populate the drink name
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        //populate the drink description
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        //populate the photo
+        ImageView photo = (ImageView) findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+    }
+}
---
+++ app/src/main/java/com/hfad/starbuzz/StarBuzzDatabaseHelper.java
@@ -0,0 +1,53 @@
+package com.hfad.starbuzz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class StarBuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz"; //name of our database
+    private static final int DB_VERSION = 2; //the version of the database
+
+    //why dont the parameters from the super call match the method call???
+    public StarBuzzDatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private static void insertDrink(SQLiteDatabase db, String name, String description, int resourceId){
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME",name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drinkValues);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, " + "NAME TEXT, " + "DESCRIPTION TEXT, " + "IMAGE_RESOURCE_ID INTEGER);");
+            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "Cappuccino", "espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+            insertDrink(db,"Filter", "our best drip coffee", R.drawable.filter);
+
+        }
+
+        if (oldVersion < 2) {
+        }
+
+    }
+}
